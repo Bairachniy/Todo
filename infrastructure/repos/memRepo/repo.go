ac2d5ade381fbdd104e2c6ccfc1aab2f5f008197
Dima <bairachniy@gmail.com>
2020-05-14 13:15:26 +0300

    feat: memRepo with map

--- infrastructure/repos/memRepo/repo.go
+++ infrastructure/repos/memRepo/repo.go
@@ -1,22 +1,41 @@
 package memRepo
 
 import (
+	"fmt"
+	"sync"
 	"todomodule/app"
 	"todomodule/domain"
 )
 
+type todoMap = map[int]domain.Todo
+
 type memRepo struct {
-	todo []domain.Todo
+	todo todoMap
+	m    sync.Mutex
 }
 
 func (m memRepo) GetAll() ([]domain.Todo, error) {
-	panic("implement me")
+	todoTmp := []domain.Todo{}
+	m.m.Lock()
+	for k := range m.todo {
+		todoTmp = append(todoTmp, domain.Todo{m.todo[k].Name})
+	}
+	m.m.Unlock()
+	return todoTmp, nil
 }
 
 func (m memRepo) Create(todo domain.Todo) error {
-	panic("implement me")
+	m.m.Lock()
+	var keys []int
+	for k := range m.todo {
+		keys = append(keys, k)
+	}
+	fmt.Println(len(keys), "maps")
+	m.todo[len(keys)+1] = todo
+	m.m.Unlock()
+	return nil
 }
 
 func NewMemRepo() app.Repo {
-	return &memRepo{}
+	return &memRepo{todo: make(todoMap)}
 }
--- main.go
+++ main.go
@@ -1,35 +1,50 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"github.com/jmoiron/sqlx"
-	"log"
-	"todomodule/app"
 	"todomodule/domain"
-	"todomodule/infrastructure/repos/dbRepo"
+	"todomodule/infrastructure/repos/memRepo"
 )
 
-// TODO Добавить UNIQUE в todo
-// TODO Добавить inmmemo repo с mutex и map
+// TODO Добавить UNIQUE в todo ---------
+// TODO Добавить inmmemo repo с mutex и map --------
 // TODO Убрать хардкод defaultConnStr, defaultDriverName. Добавить поле для коннекта в config.json
 // TODO Создать package httphandlers в infractructure. В нем должен быть тип TodoHandlers struct. TodoHandlers
 // будет обрабатывать http запросы. POST на CreateTodo и Get на GetAll. Нужно запустить сервер на localhost:8080
 
-var defaultConnStr string = "user=postgres password=Qaz54321 dbname=testdb sslmode=disable"
+var defaultConnStr string = "user=postgres password=wizard dbname=postgres sslmode=disable"
 var defaultDriverName string = "postgres"
 
 func main() {
 	fmt.Println("Start program")
-	db, err := sqlx.Connect(defaultDriverName, defaultConnStr)
+	////ClearAllDb()
+	//db, err := sqlx.Connect(defaultDriverName, defaultConnStr)
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
+	//repo, err := dbRepo.NewDbRepo(db)
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
+	//service := app.NewTodos(repo)
+	//service.Create(domain.Todo{"Something first"})
+	//service.Create(domain.Todo{"Anything else"})
+	//
+	//fmt.Println(service.GetAll())
+	repo := memRepo.NewMemRepo()
+	repo.Create(domain.Todo{"First"})
+	repo.Create(domain.Todo{"Second"})
+	fmt.Println(repo.GetAll())
+
+}
+
+func ClearAllDb() {
+	connStr := "user=postgres password=wizard dbname=postgres sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		log.Fatalln(err)
+		panic(err)
 	}
-	repo, err := dbRepo.NewDbRepo(db)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	service := app.NewTodos(repo)
-	service.Create(domain.Todo{"Something first"})
-	service.Create(domain.Todo{"Anything else"})
-	fmt.Println(service.GetAll())
+	defer db.Close()
+	db.Exec("delete from todos")
 }
